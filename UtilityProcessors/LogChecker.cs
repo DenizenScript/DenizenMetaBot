260172119a1f8ef613bd4f73e920760106f733fd
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-05-22 08:52:50 -0700

    add exploitfixer to offline-fixers list

--- UtilityProcessors/LogChecker.cs
+++ UtilityProcessors/LogChecker.cs
@@ -102,7 +102,7 @@ namespace DenizenBot.UtilityProcessors
         static LogChecker()
         {
             AddReportedPlugin(SUSPICIOUS_PLUGINS, "(Login authenticator plugin)", "AuthMe", "LoginSecurity", "nLogin", "PinAuthentication", "LockLogin");
-            AddReportedPlugin(SUSPICIOUS_PLUGINS, "(Offline-fixer plugin)", "SkinsRestorer", "MySkin", "AntiJoinBot", "AJB");
+            AddReportedPlugin(SUSPICIOUS_PLUGINS, "(Offline-fixer plugin)", "SkinsRestorer", "MySkin", "AntiJoinBot", "AJB", "ExploitFixer");
             AddReportedPlugin(MESSY_PLUGINS, "- PlugMan is dangerous and will cause unpredictable issues. Remove it.", "PlugMan", "PluginManager");
             AddReportedPlugin(MESSY_PLUGINS, "- This plugin adds Below_Name scoreboards to NPCs.", "TAB");
             AddReportedPlugin(MESSY_PLUGINS, "- NPC Command plugins have never had a valid reason to exist, as there have always been better ways to do that. The modern way is <https://wiki.citizensnpcs.co/NPC_Commands>.", "CommandNPC", "CitizensCMD");
