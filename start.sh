1cf6a3f9761711f20d1b6f9c19cf623701381909
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-05 21:15:07 -0700

    fix start script error

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull origin master
 git submodule update --init --recursive
 dotnet build DenizenMetaBot.sln --configuration Release -o ./bin/live_release
-screen -dmS DenizenMetaBot dotnet bin/live_release/DenizenMetaBot.dll -- $1
+screen -dmS DenizenMetaBot dotnet bin/live_release/DenizenMetaBot.dll $1
