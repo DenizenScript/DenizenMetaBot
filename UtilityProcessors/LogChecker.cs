c105dec6e7992cf7969febd275466a7871eec457
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-19 16:34:02 -0700

    offline, no bungee, but idv4 = probably bungee

--- UtilityProcessors/LogChecker.cs
+++ UtilityProcessors/LogChecker.cs
@@ -656,7 +656,7 @@ namespace DenizenBot.UtilityProcessors
             AutoField(embed, "Plugin Version(s)", string.Join('\n', PluginVersions), blockCode: false, inline: false);
             if (IsOffline)
             {
-                AutoField(embed, "Online/Offline", IsBungee ? "Offline, but running bungee." : (IsDenizenDebug ? "Offline." : "Offline (bungee status unknown)."));
+                AutoField(embed, "Online/Offline", IsBungee ? "Offline, but running bungee." : (IsDenizenDebug ? "Offline." : (UUIDVersion == 4 ? "Offline (bungee likely)." : "Offline (bungee status unknown).")));
             }
             if (UUIDVersion != 0)
             {
