35eb8ff0a4455c4f10f0bf0ec3b6ba1892928b65
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2021-11-19 01:33:38 -0800

    LogChecker: ModelEngine note

--- UtilityProcessors/LogChecker.cs
+++ UtilityProcessors/LogChecker.cs
@@ -126,6 +126,7 @@ namespace DenizenBot.UtilityProcessors
             AddReportedEntry(MESSY_PLUGINS, "- Mixed client vs server versions can sometimes cause packet-related issues.", "ViaVersion", "ProtocolSupport");
             AddReportedEntry(MESSY_PLUGINS, "- HeadDatabase has been known to cause issues with skins.", "HeadDatabase");
             AddReportedEntry(MESSY_PLUGINS, "- CMI tends to mess with a large variety of server features and often gets in the way of issue debugging.", "CMI");
+            AddReportedEntry(MESSY_PLUGINS, "- ModelEngine has Citizens support, but that support is known to be buggy. Issues related to NPCs that use ModelEngine should be reported to ModelEngine support, not Citizens.", "ModelEngine");
             AddReportedEntry(MESSY_PLUGINS, "- 'PlayerProfiles' has been seen to cause breaking issues with Citizens.", "PlayerProfiles");
             AddReportedEntry(MESSY_PLUGINS, "- Multi-world configuration plugins may affect NPCs in unexpected ways.", "Multiverse", "Universes");
             AddReportedEntry(MESSY_PLUGINS, "- 'Lag fix' plugins usually don't actually fix lag, they just delete spawned entities. If you have issues with entities disappearing, this plugin is likely the cause.", "ClearLagg", "LaggRemover");
