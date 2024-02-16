2f39c0d47208bb56445df13bfc4984ada14a7592
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-02-16 12:59:17 -0800

    better restart command

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git pull origin master
 git submodule update --init --recursive
+rm -rf ./bin/live_release_old
+mv ./bin/live_release ./bin/live_release_old
 dotnet build DenizenMetaBot.sln --configuration Release -o ./bin/live_release
 screen -dmS DenizenMetaBot dotnet bin/live_release/DenizenMetaBot.dll $1
