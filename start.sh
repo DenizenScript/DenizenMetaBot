43f588be269600f43c0cb7acae620af377e2989e
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 22:13:15 -0800

    minor bumps and use nicer restart technique

--- start.sh
+++ start.sh
@@ -5,3 +5,8 @@ rm -rf ./bin/live_release_old
 mv ./bin/live_release ./bin/live_release_old
 dotnet build DenizenMetaBot.sln --configuration Release -o ./bin/live_release
 dotnet bin/live_release/DenizenMetaBot.dll $1
+
+# Exit code 42 means restart, anything else = don't.
+if [ $? == 42 ]; then
+    source ./start.sh
+fi
