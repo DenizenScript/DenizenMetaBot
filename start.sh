a753bb943b0e9f4a6c4b8e9ec2999664547d2e90
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 18:15:13 -0800

    bumps, and don't bake my screen command in

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ git submodule update --init --recursive
 rm -rf ./bin/live_release_old
 mv ./bin/live_release ./bin/live_release_old
 dotnet build DenizenMetaBot.sln --configuration Release -o ./bin/live_release
-screen -dmS DenizenMetaBot dotnet bin/live_release/DenizenMetaBot.dll $1
+dotnet bin/live_release/DenizenMetaBot.dll $1
